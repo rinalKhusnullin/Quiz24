ea261bb245a74738ee4b7f1deeb62af4d5e06eac
Rinal Khusnullin <fredygames66@gmail.com>
2023-04-11 00:48:36 +0200

    components/up/quiz.edit : add base template to quiz.edit page

--- install/components/up/quiz.edit/templates/.default/template.php
+++ install/components/up/quiz.edit/templates/.default/template.php
@@ -1,4 +1,42 @@
 <?php
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 ?>
-IT IS QUIZ EDIT
+
+<div class="columns">
+	<div class="column is-one-fifth question-list">
+		<div class="question-list__title">Вопросы:</div>
+		<div class="question-list__question">
+		</div>
+		<div class="question-list__question">
+		</div>
+		<div class="question-list__question">
+		</div>
+		<div class="question-list__question">
+		</div>
+		<div class="question-list__question">
+		</div>
+		<div class="question-list__question">
+		</div>
+		<div class="question-list__question">
+		</div>
+		<div class="question-list__question">
+		</div>
+		<div class="question-list__question">
+		</div>
+	</div>
+	<div class="column is-three-fifths question-preview">
+		Тут Будет превью типо : )
+	</div>
+	<div class="column question-settings">
+		<div class="question-settings__title">Настройки:</div>
+		<form action="##" method="post">
+			<div class="question-settings__input-title">Вопрос:</div>
+			<input type="text" placeholder="Введите вопрос" name="questionTitle">
+			<div class="question-settings__input-title">Тип ответа:</div>
+			<select name="questionType" id="">
+				<option value="free">Свободный ответ</option>
+				<option value="selectable">Выбираемый ответ</option>
+			</select>
+		</form>
+	</div>
+</div>
