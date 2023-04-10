434adbcc276d18bcd8b37d6a29d9dc3feb4484c0
Матвей Устинов <pegas2631@gmail.com>
2023-04-10 12:26:20 +0300

    test commit

--- install/routes/quiz.php
+++ install/routes/quiz.php
@@ -3,9 +3,11 @@
 use Bitrix\Main\Routing\Controllers\PublicPageController;
 use Bitrix\Main\Routing\RoutingConfigurator;
 
-return function (RoutingConfigurator $routes) 
+return function (RoutingConfigurator $routes)
 {
 	$routes->get('/', new PublicPageController('/local/modules/up.quiz/views/quiz-list.php'));
 
 	$routes->get('/quiz/{quizId}/', new PublicPageController('/local/modules/up.quiz/views/quiz-details.php'));
+
+	$routes->get('/quiz/{quizId}', new PublicPageController('/local/modules/up.quiz/views/quiz-details.php'));
 };
