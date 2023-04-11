0f6cb57a439372de72f29fe168211409dd33e5b4
Матвей Устинов <pegas2631@gmail.com>
2023-04-11 13:29:46 +0300

    add uninstall script

---
+++ install/db/uninstall.sql
@@ -0,0 +1,7 @@
+drop table up_quiz_answers;
+drop table up_quiz_questions;
+drop table up_quiz_question_question_types;
+drop table up_quiz_question_display_types;
+drop table up_quiz_quizzes;
+drop table up_quiz_users;
+
