01e2890e230c6bb3e3331b0d026f0a87b72dd920
Rinal Khusnullin <fredygames66@gmail.com>
2023-04-14 05:38:31 +0200

    components/up/quiz-show : add component to quiz-show

--- install/components/up/quiz.show/templates/.default/template.php
+++ install/components/up/quiz.show/templates/.default/template.php
@@ -1,4 +1,52 @@
 <?php
+
+use Bitrix\Main\Application;
+
 if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+\Bitrix\Main\UI\Extension::load('up.quiz-show');
 ?>
-Показ результатов
+
+<div id="quiz-container-root"></div>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+<script>
+	BX.ready(function() {
+
+		window.QuizShow = new Up.Quiz.QuizShow({
+			rootNodeId: 'quiz-container-root',
+			quizId : <?= Application::getInstance()->getContext()->getRequest()->get('quizId');?>
+		});
+
+	});
+
+	const ctx = document.getElementById('myChart');
+
+	let hello = new Chart(ctx, {
+		type: 'bar',
+		data: {
+			labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+			datasets: [{
+				label: '# of Votes',
+				data: [12, 19, 3, 5, 2, 3],
+				borderWidth: 1
+			}]
+		},
+		options: {
+			scales: {
+				y: {
+					beginAtZero: true
+				}
+			}
+		}
+	});
+
+	function test(){
+		for (let i = 0; i < 10; i++){
+			setTimeout(function(){
+				hello.data.datasets[0].data[0]++;
+				hello.update();
+			}, 1);
+		}
+	}
+</script>
