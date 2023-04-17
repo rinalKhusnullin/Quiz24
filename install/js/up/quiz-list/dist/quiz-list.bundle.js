f31eecfef09c86056c0ddf14c6ca7f9c60e956b1
Rinal Khusnullin <fredygames66@gmail.com>
2023-04-17 07:31:54 +0200

    js/up/quiz-list : add is_active, validate creating quiz input and little things

--- install/js/up/quiz-list/dist/quiz-list.bundle.js
+++ install/js/up/quiz-list/dist/quiz-list.bundle.js
@@ -2,7 +2,7 @@ this.Up = this.Up || {};
 (function (exports,main_core) {
 	'use strict';
 
-	var _templateObject, _templateObject2;
+	var _templateObject, _templateObject2, _templateObject3, _templateObject4, _templateObject5;
 	var QuizList = /*#__PURE__*/function () {
 	  function QuizList() {
 	    var options = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
@@ -16,6 +16,7 @@ this.Up = this.Up || {};
 	    if (!this.rootNode) {
 	      throw new Error("QuizList: element with id \"".concat(this.rootNodeId, "\" not found"));
 	    }
+	    this.userId = 1;
 	    this.quizList = [];
 	    this.reload();
 	  }
@@ -32,8 +33,13 @@ this.Up = this.Up || {};
 	    key: "loadList",
 	    value: function loadList() {
 	      this.renderLoading();
+	      var UserId = 1;
 	      return new Promise(function (resolve, reject) {
-	        BX.ajax.runAction('up:quiz.quiz.getList').then(function (response) {
+	        BX.ajax.runAction('up:quiz.quiz.getList', {
+	          data: {
+	            userId: UserId
+	          }
+	        }).then(function (response) {
 	          var quizList = response.data.quizList;
 	          resolve(quizList);
 	        })["catch"](function (error) {
@@ -45,29 +51,29 @@ this.Up = this.Up || {};
 	  }, {
 	    key: "createQuiz",
 	    value: function createQuiz(title) {
-	      var _this2 = this;
-	      this.renderLoading();
-	      var UserId = 1;
 	      BX.ajax.runAction('up:quiz.quiz.createQuiz', {
 	        data: {
 	          title: title,
-	          id: UserId
+	          userId: this.userId
 	        }
 	      }).then(function (response) {
-	        if (response.data != null) {
-	          //check response
-	          console.error('errors:', response.data);
-	        } else {
-	          _this2.reload();
+	        if (Number.isInteger(response.data)) {
+	          window.location.href = "/quiz/".concat(response.data, "/edit");
 	        }
 	      })["catch"](function (error) {
-	        console.error(error);
+	        var errors = error.errors;
+	        errors.forEach(function (error) {
+	          if (error.code === 'invalid_user_id') {
+	            alert('TODO:НЕПРАВИЛЬНЫЙ USER_ID');
+	          }
+	        });
+	        console.log(error);
 	      });
 	    }
 	  }, {
 	    key: "deleteQuiz",
 	    value: function deleteQuiz(id) {
-	      var _this3 = this;
+	      var _this2 = this;
 	      this.renderLoading();
 	      BX.ajax.runAction('up:quiz.quiz.deleteQuiz', {
 	        data: {
@@ -77,7 +83,7 @@ this.Up = this.Up || {};
 	        if (response.data != null) {
 	          console.error('errors:', response.data);
 	        } else {
-	          _this3.reload();
+	          _this2.reload();
 	        }
 	      })["catch"](function (error) {
 	        console.error(error);
@@ -91,35 +97,54 @@ this.Up = this.Up || {};
 	  }, {
 	    key: "render",
 	    value: function render() {
-	      var _this4 = this;
+	      var _this3 = this;
 	      this.rootNode.innerHTML = "";
-	      var QuizContainerNode = main_core.Tag.render(_templateObject || (_templateObject = babelHelpers.taggedTemplateLiteral(["\n\t\t\t<div class=\"quiz-container\">\n\t\t\t\t<div class=\"quiz-card quiz-card__add-new\">\n\t\t\t\t\t<a class=\"is-success is-button quiz-card__new-quiz-btn\" id=\"open_creating_modal_btn\">\n\t\t\t\t\t\t<i class=\"fa-solid fa-plus\"></i>\n\t\t\t\t\t</a>\n\t\t\t\t\t<div class=\"modal\" id=\"new_quiz_modal\">\n\t\t\t\t\t\t<div class=\"modal-background close-modal\"></div>\n\t\t\t\t\t\t<div class=\"modal-card\">\n\t\t\t\t\t\t\t<header class=\"modal-card-head\">\n\t\t\t\t\t\t\t\t<p class=\"modal-card-title\">\u0421\u043E\u0437\u0434\u0430\u043D\u0438\u0435 \u043E\u043F\u0440\u043E\u0441\u0430</p>\n\t\t\t\t\t\t\t\t<button class=\"delete close-modal\" aria-label=\"close\"></button>\n\t\t\t\t\t\t\t</header>\n\t\t\t\t\t\t\t<section class=\"modal-card-body is-dark\">\n\t\t\t\t\t\t\t\t<div class=\"field\">\n\t\t\t\t\t\t\t\t\t<label class=\"label\">\u041D\u0430\u0437\u0432\u0430\u043D\u0438\u0435 \u043E\u043F\u0440\u043E\u0441\u0430</label>\n\t\t\t\t\t\t\t\t\t<div class=\"control\">\n\t\t\t\t\t\t\t\t\t\t<input id=\"quizTitle\" class=\"input\" type=\"text\" placeholder=\"\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u043D\u0430\u0437\u0432\u0430\u043D\u0438\u0435 \u043E\u043F\u0440\u043E\u0441\u0430\">\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t</section>\n\t\t\t\t\t\t\t<footer class=\"modal-card-foot\">\n\t\t\t\t\t\t\t\t<button class=\"button is-success\" id=\"creating_quiz_btn\">\u0421\u043E\u0437\u0434\u0430\u0442\u044C</button>\n\t\t\t\t\t\t\t\t<button class=\"button close-modal\">Cancel</button>\n\t\t\t\t\t\t\t</footer>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t"])));
+	      var QuizContainerNode = main_core.Tag.render(_templateObject || (_templateObject = babelHelpers.taggedTemplateLiteral(["\n\t\t\t<div class=\"quiz-container\">\n\t\t\t\t<div class=\"quiz-card quiz-card__add-new\">\n\t\t\t\t\t<a class=\"is-success is-button quiz-card__new-quiz-btn\" id=\"open_creating_modal_btn\">\n\t\t\t\t\t\t<i class=\"fa-solid fa-plus\"></i>\n\t\t\t\t\t</a>\n\t\t\t\t\t<div class=\"modal\" id=\"new_quiz_modal\">\n\t\t\t\t\t\t<div class=\"modal-background close-modal\"></div>\n\t\t\t\t\t\t<div class=\"modal-card\">\n\t\t\t\t\t\t\t<header class=\"modal-card-head\">\n\t\t\t\t\t\t\t\t<p class=\"modal-card-title\">\u0421\u043E\u0437\u0434\u0430\u043D\u0438\u0435 \u043E\u043F\u0440\u043E\u0441\u0430</p>\n\t\t\t\t\t\t\t\t<button class=\"delete close-modal\" aria-label=\"close\"></button>\n\t\t\t\t\t\t\t</header>\n\t\t\t\t\t\t\t<section class=\"modal-card-body is-dark\">\n\t\t\t\t\t\t\t\t<div class=\"field\">\n\t\t\t\t\t\t\t\t\t<label class=\"label\">\u041D\u0430\u0437\u0432\u0430\u043D\u0438\u0435 \u043E\u043F\u0440\u043E\u0441\u0430</label>\n\t\t\t\t\t\t\t\t\t<div class=\"control\">\n\t\t\t\t\t\t\t\t\t\t<input id=\"quizTitle\" class=\"input\" type=\"text\" placeholder=\"\u0412\u0432\u0435\u0434\u0438\u0442\u0435 \u043D\u0430\u0437\u0432\u0430\u043D\u0438\u0435 \u043E\u043F\u0440\u043E\u0441\u0430\">\n\t\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t\t\t<p class=\"help\" id=\"creating-quiz-helper\"></p>\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t</section>\n\t\t\t\t\t\t\t<footer class=\"modal-card-foot\">\n\t\t\t\t\t\t\t\t<button class=\"button is-success\" id=\"creating_quiz_btn\">\u0421\u043E\u0437\u0434\u0430\u0442\u044C</button>\n\t\t\t\t\t\t\t\t<button class=\"button close-modal\">Cancel</button>\n\t\t\t\t\t\t\t</footer>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t</div>\n\t\t"])));
 	      this.quizList.forEach(function (QuizData) {
-	        var QuizCard = main_core.Tag.render(_templateObject2 || (_templateObject2 = babelHelpers.taggedTemplateLiteral(["\n\t\t\t\t<div class=\"quiz-card\" data-quiz-id=\"", "\">\n\t\t\t\t\t<div class=\"quiz-card__header\"></div>\n\t\t\t\t\t\t<div class=\"quiz-card__content\">\n\t\t\t\t\t\t\t<div class=\"quiz-card__title\">\n\t\t\t\t\t\t\t\t<strong class=\"quiz-card__subtitle is-family-monospace\">\u041D\u0430\u0437\u0432\u0430\u043D\u0438\u0435:</strong>\n\t\t\t\t\t\t\t\t<div class=\"quiz-card__title-text has-text-weight-light\">\n\t\t\t\t\t\t\t\t\t", "\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"quiz-card__title\">\n\t\t\t\t\t\t\t<strong class=\"quiz-card__subtitle is-family-monospace\">linkcode:</strong>\n\t\t\t\t\t\t\t<div class=\"quiz-card__title-text has-text-weight-light\">\n\t\t\t\t\t\t\t\t", "\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"quiz-card__hidden-btns\">\n\t\t\t\t\t\t<a href=\"/quiz/", "/edit\" class=\"button\">\n\t\t\t\t\t\t\t<i class=\"fa-solid fa-pen\"></i>\n\t\t\t\t\t\t</a>\n\t\t\t\t\t\t<a href=\"/quiz/", "/show\" class=\"button\">\n\t\t\t\t\t\t\t<i class=\"fa-sharp fa-solid fa-chart-column\"></i>\n\t\t\t\t\t\t</a>\n\t\t\t\t\t\t<a class=\"button delete-quiz-button\" >\n\t\t\t\t\t\t\t<i class=\"fa-sharp fa-solid fa-trash\"></i>\n\t\t\t\t\t\t</a>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t"])), QuizData.ID, QuizData.TITLE, QuizData.CODE, QuizData.ID, QuizData.ID);
+	        var isActive = +QuizData.IS_ACTIVE === 1 ? 'Включен' : 'Выключен';
+	        var QuizCard = main_core.Tag.render(_templateObject2 || (_templateObject2 = babelHelpers.taggedTemplateLiteral(["\n\t\t\t\t<div class=\"quiz-card\" data-quiz-id=\"", "\">\n\t\t\t\t\t<div class=\"quiz-card__header\"></div>\n\t\t\t\t\t\t<div class=\"quiz-card__content\">\n\t\t\t\t\t\t\t<div class=\"quiz-card__title\">\n\t\t\t\t\t\t\t\t<strong class=\"quiz-card__subtitle is-family-monospace\">\u041D\u0430\u0437\u0432\u0430\u043D\u0438\u0435:</strong>\n\t\t\t\t\t\t\t\t<div class=\"quiz-card__title-text has-text-weight-light\">\n\t\t\t\t\t\t\t\t\t", "\n\t\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"quiz-card__title\">\n\t\t\t\t\t\t\t<strong class=\"quiz-card__subtitle is-family-monospace\">linkcode:</strong>\n\t\t\t\t\t\t\t<div class=\"quiz-card__title-text has-text-weight-light\">\n\t\t\t\t\t\t\t\t", "\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t\t<div class=\"quiz-card__title\">\n\t\t\t\t\t\t\t<strong class=\"quiz-card__subtitle is-family-monospace\">\u0421\u043E\u0441\u0442\u043E\u044F\u043D\u0438\u0435:</strong>\n\t\t\t\t\t\t\t<div class=\"quiz-card__title-text has-text-weight-light\">\n\t\t\t\t\t\t\t\t", "\n\t\t\t\t\t\t\t</div>\n\t\t\t\t\t\t</div>\n\t\t\t\t\t</div>\n\t\t\t\t\t<div class=\"quiz-card__hidden-btns\">\n\t\t\t\t\t\t", "\n\t\t\t\t\t\t<a href=\"/quiz/", "/edit\" title=\"\u0420\u0435\u0434\u0430\u043A\u0442\u0438\u0440\u043E\u0432\u0430\u0442\u044C \u043E\u043F\u0440\u043E\u0441\">\n\t\t\t\t\t\t\t<i class=\"fa-solid fa-pen fa-fw\"></i>\n\t\t\t\t\t\t</a>\n\t\t\t\t\t\t<a href=\"/quiz/", "/show\" title=\"\u041F\u043E\u043A\u0430\u0437\u0430\u0442\u044C \u0440\u0435\u0437\u0443\u043B\u044C\u0442\u0430\u0442\u044B\">\n\t\t\t\t\t\t\t<i class=\"fa-sharp fa-solid fa-chart-column fa-fw\"></i>\n\t\t\t\t\t\t</a>\n\t\t\t\t\t\t<a href=\"/quiz/", "/show\" title=\"\u041F\u043E\u0434\u0435\u043B\u0438\u0442\u044C\u0441\u044F\">\n\t\t\t\t\t\t\t<i class=\"fa-solid fa-link fa-fw\"></i>\n\t\t\t\t\t\t</a>\n\t\t\t\t\t\t<a class=\"delete-quiz-button\" title=\"\u0423\u0434\u0430\u043B\u0438\u0442\u044C \u043E\u043F\u0440\u043E\u0441\">\n\t\t\t\t\t\t\t<i class=\"fa-sharp fa-solid fa-trash fa-fw\"></i>\n\t\t\t\t\t\t</a>\n\t\t\t\t\t</div>\n\t\t\t\t</div>\n\t\t\t"])), QuizData.ID, QuizData.TITLE, QuizData.CODE, isActive, _this3.getStateButton(QuizData), QuizData.ID, QuizData.ID, QuizData.ID);
 	        QuizContainerNode.appendChild(QuizCard);
 	      });
 	      this.rootNode.appendChild(QuizContainerNode);
 	      var openModalButton = document.getElementById('open_creating_modal_btn');
 	      openModalButton.addEventListener('click', function () {
-	        _this4.openCreatingQuizModal();
+	        _this3.openCreatingQuizModal();
 	      });
 	      var closeModalElems = document.querySelectorAll('.close-modal');
 	      closeModalElems.forEach(function (closeModalElem) {
 	        closeModalElem.addEventListener('click', function () {
-	          _this4.closeCreatingQuizModal();
+	          _this3.closeCreatingQuizModal();
 	        });
 	      });
 	      var addButton = document.getElementById('creating_quiz_btn');
 	      addButton.addEventListener('click', function () {
 	        var quizTitle = document.getElementById('quizTitle').value;
-	        _this4.createQuiz(quizTitle);
+	        if (quizTitle !== '') _this3.createQuiz(quizTitle);
+	      });
+	      var QuizTitleInput = document.getElementById('quizTitle');
+	      QuizTitleInput.addEventListener('input', function () {
+	        var QuizTitleInputHelper = document.getElementById('creating-quiz-helper');
+	        if (QuizTitleInput.value === '') {
+	          // Я знаю, что это ужасно! Думаю вынести в отдельную функцию
+	          QuizTitleInput.classList.add('is-danger');
+	          QuizTitleInput.classList.remove('is-success');
+	          QuizTitleInputHelper.textContent = 'Название опроса не может быть пустым!';
+	          QuizTitleInputHelper.classList.add('is-danger');
+	          QuizTitleInputHelper.classList.remove('is-success');
+	        } else {
+	          QuizTitleInput.classList.add('is-success');
+	          QuizTitleInput.classList.remove('is-danger');
+	          QuizTitleInputHelper.textContent = 'Все кул!';
+	          QuizTitleInputHelper.classList.remove('is-danger');
+	          QuizTitleInputHelper.classList.add('is-success');
+	        }
 	      });
 	      var deleteButtons = document.querySelectorAll('.delete-quiz-button');
 	      deleteButtons.forEach(function (button) {
 	        button.addEventListener('click', function () {
 	          var quizId = parseInt(button.closest('.quiz-card').getAttribute('data-quiz-id'));
 	          if (!isNaN(quizId)) {
-	            _this4.deleteQuiz(quizId);
+	            _this3.deleteQuiz(quizId);
 	          } else {
 	            console.error('Attribute data-quiz-id of this element is not a number ');
 	          }
@@ -137,6 +162,42 @@ this.Up = this.Up || {};
 	    value: function closeCreatingQuizModal() {
 	      var modal = document.getElementById('new_quiz_modal');
 	      modal.classList.remove("is-active");
+	    }
+	  }, {
+	    key: "getStateButton",
+	    value: function getStateButton(quiz) {
+	      var _this4 = this;
+	      var button = main_core.Tag.render(_templateObject3 || (_templateObject3 = babelHelpers.taggedTemplateLiteral(["<a></a>"])));
+	      if (+quiz.IS_ACTIVE === 0) {
+	        button.title = 'Запустить опрос';
+	        button.appendChild(main_core.Tag.render(_templateObject4 || (_templateObject4 = babelHelpers.taggedTemplateLiteral(["<i class=\"fa-sharp fa-regular fa-circle-play fa-fw\"></i>"]))));
+	      } else {
+	        button.title = 'Выключить опрос';
+	        button.appendChild(main_core.Tag.render(_templateObject5 || (_templateObject5 = babelHelpers.taggedTemplateLiteral(["<i class=\"fa-sharp fa-regular fa-circle-stop fa-fw\"></i>"]))));
+	      }
+	      button.onclick = function () {
+	        _this4.changeState(quiz.ID);
+	      };
+	      return button;
+	    }
+	  }, {
+	    key: "changeState",
+	    value: function changeState(id) {
+	      var _this5 = this;
+	      BX.ajax.runAction('up:quiz.quiz.changeState', {
+	        data: {
+	          id: id
+	        }
+	      }).then(function (response) {
+	        if (response.data.quizId === null) {
+	          //check response
+	          console.error('errors:', response.data);
+	        } else {
+	          _this5.reload();
+	        }
+	      })["catch"](function (error) {
+	        console.error(error);
+	      });
 	    }
 	  }]);
 	  return QuizList;
--- install/js/up/quiz-list/src/quiz-list.js
+++ install/js/up/quiz-list/src/quiz-list.js
@@ -19,6 +19,8 @@ export class QuizList
 			throw new Error(`QuizList: element with id "${this.rootNodeId}" not found`);
 		}
 
+		this.userId = 1;
+
 		this.quizList = [];
 		this.reload();
 	}
@@ -35,9 +37,14 @@ export class QuizList
 	loadList()
 	{
 		this.renderLoading();
+		let UserId = 1;
 		return new Promise((resolve, reject) => {
 			BX.ajax.runAction(
-					'up:quiz.quiz.getList',
+					'up:quiz.quiz.getList',{
+						data : {
+							userId : UserId,
+						}
+					}
 				)
 				.then((response) => {
 					const quizList = response.data.quizList;
@@ -53,29 +60,28 @@ export class QuizList
 
 	createQuiz(title)
 	{
-		this.renderLoading();
-		let UserId = 1;
 		BX.ajax.runAction(
 				'up:quiz.quiz.createQuiz',
 				{
 					data: {
 						title: title,
-						id: UserId
+						userId: this.userId,
 					},
 				})
 			.then((response) => {
-				if (response.data != null)
-				{
-					//check response
-					console.error('errors:', response.data);
-				}
-				else
-				{
-					this.reload();
+				if (Number.isInteger(response.data)){
+					window.location.href = `/quiz/${response.data}/edit`;
 				}
 			})
 			.catch((error) => {
-				console.error(error);
+				let errors = error.errors;
+				errors.forEach(error => {
+					if (error.code === 'invalid_user_id'){
+						alert('TODO:НЕПРАВИЛЬНЫЙ USER_ID');
+					}
+				});
+
+				console.log(error);
 			})
 		;
 	}
@@ -135,6 +141,7 @@ export class QuizList
 									<div class="control">
 										<input id="quizTitle" class="input" type="text" placeholder="Введите название опроса">
 									</div>
+									<p class="help" id="creating-quiz-helper"></p>
 								</div>
 							</section>
 							<footer class="modal-card-foot">
@@ -148,6 +155,7 @@ export class QuizList
 		`;
 
 		this.quizList.forEach(QuizData => {
+			let isActive = (+QuizData.IS_ACTIVE === 1) ? 'Включен' : 'Выключен';
 			const QuizCard = Tag.render`
 				<div class="quiz-card" data-quiz-id="${QuizData.ID}">
 					<div class="quiz-card__header"></div>
@@ -164,16 +172,26 @@ export class QuizList
 								${QuizData.CODE}
 							</div>
 						</div>
+						<div class="quiz-card__title">
+							<strong class="quiz-card__subtitle is-family-monospace">Состояние:</strong>
+							<div class="quiz-card__title-text has-text-weight-light">
+								${isActive}
+							</div>
+						</div>
 					</div>
 					<div class="quiz-card__hidden-btns">
-						<a href="/quiz/${QuizData.ID}/edit" class="button">
-							<i class="fa-solid fa-pen"></i>
+						${this.getStateButton(QuizData)}
+						<a href="/quiz/${QuizData.ID}/edit" title="Редактировать опрос">
+							<i class="fa-solid fa-pen fa-fw"></i>
 						</a>
-						<a href="/quiz/${QuizData.ID}/show" class="button">
-							<i class="fa-sharp fa-solid fa-chart-column"></i>
+						<a href="/quiz/${QuizData.ID}/show" title="Показать результаты">
+							<i class="fa-sharp fa-solid fa-chart-column fa-fw"></i>
 						</a>
-						<a class="button delete-quiz-button" >
-							<i class="fa-sharp fa-solid fa-trash"></i>
+						<a href="/quiz/${QuizData.ID}/show" title="Поделиться">
+							<i class="fa-solid fa-link fa-fw"></i>
+						</a>
+						<a class="delete-quiz-button" title="Удалить опрос">
+							<i class="fa-sharp fa-solid fa-trash fa-fw"></i>
 						</a>
 					</div>
 				</div>
@@ -197,7 +215,27 @@ export class QuizList
 		const addButton = document.getElementById('creating_quiz_btn');
 		addButton.addEventListener('click', () => {
 			let quizTitle = document.getElementById('quizTitle').value;
-			this.createQuiz(quizTitle);
+			if (quizTitle !== '') this.createQuiz(quizTitle);
+		});
+
+		const QuizTitleInput = document.getElementById('quizTitle');
+		QuizTitleInput.addEventListener('input', () => {
+			const QuizTitleInputHelper = document.getElementById('creating-quiz-helper');
+			if (QuizTitleInput.value === ''){ // Я знаю, что это ужасно! Думаю вынести в отдельную функцию
+				QuizTitleInput.classList.add('is-danger');
+				QuizTitleInput.classList.remove('is-success');
+				QuizTitleInputHelper.textContent = 'Название опроса не может быть пустым!'
+				QuizTitleInputHelper.classList.add('is-danger');
+				QuizTitleInputHelper.classList.remove('is-success');
+			}
+			else
+			{
+				QuizTitleInput.classList.add('is-success');
+				QuizTitleInput.classList.remove('is-danger');
+				QuizTitleInputHelper.textContent = 'Все кул!'
+				QuizTitleInputHelper.classList.remove('is-danger');
+				QuizTitleInputHelper.classList.add('is-success');
+			}
 		});
 
 		const deleteButtons = document.querySelectorAll('.delete-quiz-button');
@@ -226,5 +264,52 @@ export class QuizList
 	{
 		const modal = document.getElementById('new_quiz_modal');
 		modal.classList.remove("is-active");
+	}
+
+	getStateButton(quiz)
+	{
+		const button = Tag.render`<a></a>`;
+		if (+quiz.IS_ACTIVE === 0)
+		{
+			button.title = 'Запустить опрос';
+			button.appendChild(Tag.render`<i class="fa-sharp fa-regular fa-circle-play fa-fw"></i>`);
+		}
+		else
+		{
+			button.title = 'Выключить опрос';
+			button.appendChild(Tag.render`<i class="fa-sharp fa-regular fa-circle-stop fa-fw"></i>`);
+		}
+
+		button.onclick = () => {
+			this.changeState(quiz.ID);
+		};
+
+		return button;
+	}
+
+	changeState(id)
+	{
+		BX.ajax.runAction(
+				'up:quiz.quiz.changeState',
+				{
+					data: {
+						id : id
+					},
+				})
+			.then((response) => {
+				if (response.data.quizId === null)
+				{
+					//check response
+					console.error('errors:', response.data);
+				}
+				else
+				{
+					this.reload();
+				}
+			})
+			.catch((error) => {
+				console.error(error);
+			})
+		;
 	}
 }
