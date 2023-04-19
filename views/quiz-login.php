28139fac410b6375d5db0045af0592ffe8d8c2bd
Rinal Khusnullin <fredygames66@gmail.com>
2023-04-19 17:18:53 +0200

    views : add views for quiz-login and quiz-registration

---
+++ views/quiz-login.php
@@ -0,0 +1,11 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+
+$APPLICATION->SetTitle("Login");
+
+$APPLICATION->includeComponent('up:quiz.login', '', []);
+
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
+?>
