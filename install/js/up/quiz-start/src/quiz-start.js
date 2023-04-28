f7c4ab3084ba5207bf891184890fa2e48368cc18
Матвей Устинов <pegas2631@gmail.com>
2023-04-28 17:42:48 +0300

    quiz-start loc

--- install/js/up/quiz-start/src/quiz-start.js
+++ install/js/up/quiz-start/src/quiz-start.js
@@ -1,4 +1,4 @@
-import {Type, Tag} from 'main.core';
+import {Type, Tag, Text, Loc} from 'main.core';
 
 export class QuizStart
 {
@@ -31,20 +31,20 @@ export class QuizStart
 	{
 		const StartContainerNode = Tag.render`
 			<div class="box not-auth">
-				<h1 class="title is-4">Укажите код опроса, в котором хотите принять участие</h1>
+				<h1 class="title is-4">${Loc.getMessage('UP_QUIZ_START_MESSAGE')}</h1>
 					<div class="field has-addons has-addons-centered">
 						<div class="control">
-							<input class="input is-dark" type="text" id="quiz-code-input" placeholder="Введите quiz-code">
+							<input class="input is-dark" type="text" id="quiz-code-input" placeholder="${Loc.getMessage('UP_QUIZ_START_ENTER_CODE')}">
 						</div>
 						<div class="control">
 							<a class="button is-dark" id="take-button">
-								Пройти опрос
+								${Loc.getMessage('UP_QUIZ_START_TAKE_QUIZ')}
 							</a>
 						</div>
 					</div>
-				<strong>ИЛИ</strong>
+				<strong>${Loc.getMessage('UP_QUIZ_START_OR')}</strong>
 				<div>
-					Выполните <a class="is-underlined" href="/login">вход</a>, для возможности создавать опросы.
+					${Loc.getMessage('UP_QUIZ_START_COMPLETE')} <a class="is-underlined" href="/login">${Loc.getMessage('UP_QUIZ_START_LOGIN')}</a>, ${Loc.getMessage('UP_QUIZ_START_TO_BE_ABLE_TO_CREATE_QUIZZES')}.
 				</div>
 			</div>
 		`;
